be7b21e4fe4615b3e57c91a93d69ec9b6a6f4ce4
David Burrell <d.burrell698@gmail.com>
2021-10-08 13:01:08 -0400

    initial commit - client

---
+++ client/src/pages/HomePage.js
@@ -0,0 +1,14 @@
+
+
+
+function HomePage() {
+
+    return(
+        <div>
+            <h1>Home Page</h1>
+            <p>Basic React setup. Feel free to change as needed</p>
+        </div>
+    )
+}
+
+export default HomePage
