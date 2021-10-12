d74e235ac1f5d3ba923cb322382b525ef0e29887
Janelle Shines <janellesh08@gmail.com>
2021-10-12 14:11:42 -0400

    added CSS to menu and updated links

--- client/src/store/reducer.js
+++ client/src/store/reducer.js
@@ -1,7 +1,15 @@
-function reducer(){
-    return(
-        <h1>reducer</h1>
-    )
+const initialState = {
+    isAuthenticated: false
+}
+
+function reducer(state = initialState, action) {
+    if (action.type == 'ON_LOGIN') {
+        return {
+            ...state,
+            isAuthenticated: true
+        }
+    }
+
 }
 
 export default reducer;
