a942a8d6ea31c0b44aaf86bf41a12536f380ead9
Janelle Shines <janellesh08@gmail.com>
2021-10-13 11:42:42 -0400

    login signup updates

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,10 +1,12 @@
 import {useState} from 'react'
+import {connect} from 'react-redux'
 
-function Login () {
+
+function Login (props) {
     const [user, setLogin] = useState({
         email:'',
+        firstName:'',
         password:'',
-        id:'',
     })
 
     const [errormsg, setErrormsg] = useState ()
@@ -26,12 +28,15 @@ function Login () {
         }).then((response) => response.json())
             .then(result => {
                 console.log(result);
-                if(!result.success) {
-                    setErrormsg("Wrong useremail/password combination!")
+                if(result.success == true) {
+                    const token = result.token
+                    localStorage.setItem('jsonwebtoken', token)
+                    localStorage.setItem('firstName', result.user.first_name)
+                    props.onLogin()
+                    props.history.push('/MyEventures')
+                    console.log("login was successful!")
                 }else{
-                    console.log('login was successful!')
-                console.log(result)
-                setErrormsg()
+                    setErrormsg('Worng user email or password')
                 }
             })
     }
@@ -39,7 +44,7 @@ function Login () {
   return (
       <div>
           <label>User Email</label>
-            <input name='user_email' type = 'text' placeholder = 'Enter User Email Here' onChange={handleOnChange}/>
+            <input name='email' type = 'text' placeholder = 'Enter User Email Here' onChange={handleOnChange}/>
           <label>Password</label>
             <input name = 'password' type = 'password' placeholder ='Enter Password' onChange={handleOnChange}/>
           <button onClick={login}>Login</button>
@@ -49,4 +54,17 @@ function Login () {
 
 }
 
-export default Login
+const mapDispatchToProps = (dispatch) => {
+    return{
+        onLogin: () => dispatch ({type:'ON_LOGIN'})
+
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        ON_LOGIN: state.firstName
+    }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(Login)
