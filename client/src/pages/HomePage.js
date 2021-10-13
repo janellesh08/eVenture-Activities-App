5d1b386036c6158a54a2815bb36bec0978cec677
Janelle Shines <janellesh08@gmail.com>
2021-10-13 10:38:14 -0400

    config and models

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,5 +1,6 @@
 import { Navbar, Nav, Container, Row, Col, CardGroup, Card } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Display3Activities from '../components/Display3Activities';
 
 
 
@@ -9,54 +10,7 @@ function HomePage() {
 
     return (
         <div>
-            <Container>
-                <Row>
-                    <Col  fluid> <h1 id="welcomeText">Welcome!</h1></Col>
-                </Row>
-                <CardGroup id="cardGroup">
-                    <Card>
-                        <Card.Img variant="top" src="holder.js/100px160" />
-                        <Card.Body>
-                            <Card.Title>Card title</Card.Title>
-                            <Card.Text>
-                                This is a wider card with supporting text below as a natural lead-in to
-                                additional content. This content is a little bit longer.
-                            </Card.Text>
-                        </Card.Body>
-                        <Card.Footer>
-                            <small className="text-muted">Last updated 3 mins ago</small>
-                        </Card.Footer>
-                    </Card>
-                    <Card>
-                        <Card.Img variant="top" src="holder.js/100px160" />
-                        <Card.Body>
-                            <Card.Title>Card title</Card.Title>
-                            <Card.Text>
-                                This card has supporting text below as a natural lead-in to additional
-                                content.{' '}
-                            </Card.Text>
-                        </Card.Body>
-                        <Card.Footer>
-                            <small className="text-muted">Last updated 3 mins ago</small>
-                        </Card.Footer>
-                    </Card>
-                    <Card>
-                        <Card.Img variant="top" src="holder.js/100px160" />
-                        <Card.Body>
-                            <Card.Title>Card title</Card.Title>
-                            <Card.Text>
-                                This is a wider card with supporting text below as a natural lead-in to
-                                additional content. This card has even longer content than the first to
-                                show that equal height action.
-                            </Card.Text>
-                        </Card.Body>
-                        <Card.Footer>
-                            <small className="text-muted">Last updated 3 mins ago</small>
-                        </Card.Footer>
-                    </Card>
-                </CardGroup>
-            </Container>
-
+            <Display3Activities/>
         </div>
     )
 }
