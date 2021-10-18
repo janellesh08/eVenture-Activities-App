4487463f71ee2c8ea4b60c150029198cbc9bdb9d
David Burrell <d.burrell698@gmail.com>
2021-10-18 12:57:57 -0400

    completed logout functionality

--- client/src/store/creators/actionCreators.js
+++ client/src/store/creators/actionCreators.js
@@ -39,3 +39,10 @@ export const submitToMyActivities = (myActivity) => {
         
     }
 }
+
+export const isLoggedOut = () => {
+    return{
+        type: actionType.ON_LOGOUT,
+        payload: false
+    }
+}
